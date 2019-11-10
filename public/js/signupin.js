efd47346ddb9cd54d918bd4e390da64b83412bee
Haram, Jung <jhr2013@gmail.com>
2019-11-10 16:30:55 +0900

    update

--- public/js/signupin.js
+++ public/js/signupin.js
@@ -1,13 +1,39 @@
 $(document).on('click','.sign-in-up-content-input-container .eye-icon',function(){
-    if($(this).hasClass('fa-eye')) {
-        $(this).removeClass('fa-eye');
-        $(this).addClass('fa-eye-slash');
-        $(this).parent().find('input').attr('type','password');
+    if($('.register-pw-input').hasClass('fa-eye')) {
+        $('.register-pw-input').removeClass('fa-eye');
+        $('.register-pw-input').addClass('fa-eye-slash');
+        $('.register-pw-input').parent().find('input').attr('type','password');
     } else {
-        $(this).removeClass('fa-eye-slash')
-        $(this).addClass('fa-eye')
-        $(this).parent().find('input').attr('type','text');
+        $('.register-pw-input').removeClass('fa-eye-slash')
+        $('.register-pw-input').addClass('fa-eye')
+        $('.register-pw-input').parent().find('input').attr('type','text');
     }
 })
 
-$(document).on('keyup keydown','')
+$(document).on('keyup keydown','.register-pw-input',function(){
+    if($('#register-pw-input1').val()!=$('#register-pw-input2').val()){
+        if($(this).parent().find('.fa-unlock-alt').hasClass('register-pw-input-green')){
+            $('#register-pw-input1').parent().find('.fa-unlock-alt').removeClass('register-pw-input-green');
+            $('#register-pw-input1').parent().find('.fa-unlock-alt').addClass('register-pw-input-red');
+            $('#register-pw-input2').parent().find('.fa-unlock-alt').removeClass('register-pw-input-green');
+            $('#register-pw-input2').parent().find('.fa-unlock-alt').addClass('register-pw-input-red');
+            $('#sign-in-up-content-input-pw-msg').slideDown()
+        }
+    }else {
+        if($(this).parent().find('.fa-unlock-alt').hasClass('register-pw-input-red')){
+            $('#register-pw-input1').parent().find('.fa-unlock-alt').removeClass('register-pw-input-red');
+            $('#register-pw-input1').parent().find('.fa-unlock-alt').addClass('register-pw-input-green');
+            $('#register-pw-input2').parent().find('.fa-unlock-alt').removeClass('register-pw-input-red');
+            $('#register-pw-input2').parent().find('.fa-unlock-alt').addClass('register-pw-input-green');
+            $('#sign-in-up-content-input-pw-msg').slideUp()
+        }
+    }
+})
+
+$(document).on('focusin focusout', ".sign-in-up-content-input", function(e){
+    console.log($(this).parent().parent());
+    if(e.type == "focusin")
+        $(this).parent().parent().find('.sign-in-up-content-input-msg').slideDown()
+    else
+        $(this).parent().parent().find('.sign-in-up-content-input-msg').slideUp()
+})
